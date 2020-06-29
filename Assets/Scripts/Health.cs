bd6f11402d614957889f03b43d3f45b78885bda2
alupoiu <alexanderlupoiu@gmail.com>
2020-06-29 15:14:58 -0700

    Basic project

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is attached to everything with health
+public class Health : MonoBehaviour
+{
+    // health of entity
+    public float health = 100f;
+
+    // Enitity that holds health
+    public GameObject entity;
+
+    void Update()
+    {
+        // Kill entity when health is less than of equal to 0
+        if (health <= 0)
+        {
+            Destroy(entity);
+        }
+    }
+}
---
+++ Assets/Scripts/Player Controller/shooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    // Bullet spawnpoint
+    public Transform gunPoint;
+    public GameObject bulletPrefab;
+    // Force added to bullet
+    public float bulletspeed = 15f;
+    // Delay until bullet deletes
+    public float delay = 5f;
+
+    void Update()
+    {
+        // Shoot when fire button is pressed
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // Instantiate bullet at the spawnpoint and add an upwards force
+        GameObject bullet = Instantiate(bulletPrefab, gunPoint.position, gunPoint.rotation);
+        Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
+        bulletRB.AddForce(- gunPoint.up * bulletspeed, ForceMode2D.Impulse);
+        // Destroy the bullet after the delay
+        Object.Destroy(bullet, delay);
+    }
+}
